61c3e898ec3d1a97d5b89e4876d84bcaed1221e5
DavidR81 <davidrecuero@gmail.com>
2019-09-08 20:35:23 +0200

    seguimos trabajando

---
+++ INPUT/DataFrame_clean.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import re
+import requests
+
+
+
+tsunami_Sources = pd.read_csv('../input/sources.csv', sep=",", header=0, index_col=0)
+#print(tsunami_Sources.head(10))
+
+tsunami_Sources = tsunami_Sources.drop(['HOUR','MINUTE','CAUSE','FOCAL_DEPTH','STATE/PROVINCE','MAGNITUDE_ABE',
+                                        'MAGNITUDE_IIDA','WARNING_STATUS','MISSING','MISSING_ESTIMATE','INJURIES',
+                                        'INJURY_ESTIMATE','DAMAGE_MILLIONS_DOLLARS','HOUSES_DAMAGED','HOUSE_DAMAGE_ESTIMATE',
+                                        'HOUSES_DESTROYED','HOUSE_DAMAGE_ESTIMATE','ALL_MISSING','MISSING_TOTAL','ALL_INJURIES',
+                                        'INJURY_TOTAL','ALL_HOUSES_DAMAGED','HOUSE_DAMAGE_TOTAL','ALL_HOUSES_DESTROYED',
+                                        'HOUSE_DESTRUCTION_TOTAL'], 1)
+
+print(tsunami_Sources.shape)
+
+
+
+
+
+
+
+
+
+    
